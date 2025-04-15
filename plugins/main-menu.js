8b25b7d501c36150c1edba1ae29fd9ba28154d01
moiz339 <maaahead181@gmail.com>
2025-04-15 19:47:32 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -360,8 +360,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'JawadTechX',
+                        newsletterJid: '120363409500820105@newsletter',
+                        newsletterName: 'MOIZ-MD',
                         serverMessageId: 143
                     }
                 }
